caf42be4f888235f0764ef55eae7ea95ca577c05
Srinivas5 <srinivas@hashicorp.com>
2021-07-10 18:46:33 -0500

    clean up

---
+++ examples/my.auto.tfvars
@@ -0,0 +1,2 @@
+ami="ami-087c2c50437d0b80d"
+instance_type = "t2.micro"
--- examples/variables.tf
+++ examples/variables.tf
@@ -6,3 +6,7 @@ variable "instance_type" {
   description = "Type of instance to start. Updates to this field will trigger a stop/start of the EC2 instance"
   default = "t3.micro"
 }
+
+variable "instance_name" {
+  description = "Instance name"  
+}
